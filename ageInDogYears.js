348c732ff5a7ef7581f03cba94ba48afa544b978
conorkelly86 <conorkelly86@gmail.com>
2017-10-25 11:49:59 +0100

    Update ageInDogYears.js

--- ageInDogYears.js
+++ ageInDogYears.js
@@ -1,14 +1,15 @@
 
-let myAge = 30;
+let myAge = 30;                                                                 //Set variable (myAge) = 30
 
-//let earlyYears = 2;
-let earlyYears = (2 *10.5);
+let earlyYears = (2 *10.5);                                                     //Set variable (earlyYears) = (2 * 10.5)
 let laterYears = myAge -= 2;
 
 
-myAgeInDogYears = earlyYears += laterYears;
+myAgeInDogYears = earlyYears += laterYears;                                     //Merge both previous variables and add
 
 const myName = 'Conor';
-'Conor'.toLowerCase();
+'Conor'.toLowerCase();                                                          //Change variable myName to output in Lower Case
 
-console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`); //Result
+
+//Output:  My name is Conor. I am 49 years old in dog years.
