dce07f5126f4089bebfd02d4ce92b8a96e73b0f8
conorkelly86 <conorkelly86@gmail.com>
2017-10-25 11:37:42 +0100

    Add files via upload

---
+++ ageInDogYears.js
@@ -0,0 +1,14 @@
+
+let myAge = 30;
+
+//let earlyYears = 2;
+let earlyYears = (2 *10.5);
+let laterYears = myAge -= 2;
+
+
+myAgeInDogYears = earlyYears += laterYears;
+
+const myName = 'Conor';
+'Conor'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
